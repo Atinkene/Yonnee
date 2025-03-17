7a55fb3939f00efed05f834c4ca96b51d23f7b1e
Atinkene <massinasylvanusbassene@gmail.com>
2025-03-17 13:13:20 +0000

    New route definition

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+Route::get('/', function () {
+    return redirect()->route('auth.connexion');
+});
 
 Route::prefix('/yonnee')->controller(App\Http\Controllers\AuthController::class)->group(function(){ 
     Route::get('/connexion/','connexion')->name('auth.connexion');
